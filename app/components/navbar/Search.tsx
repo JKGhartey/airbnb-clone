7359f828068fc82e90acb4873e40102ecec0c462
Jerome Ghartey <gharteyjerome@gmail.com>
2023-07-25 17:57:25 +0100

    luxe

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -112,7 +112,7 @@ const Search = () => {
           <div 
             className="
               p-2 
-              bg-rose-500 
+              bg-accent
               rounded-full 
               text-white
             "
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#ac5469"
+      }
+    },
   },
   plugins: [],
 }
